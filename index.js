1750f9db7dfc440c298f2d510a68ea337d5f4675
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-08-27 02:59:30 +0300

    Some Edits

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const myLeads = []
+let myLeads = []
 
-const inputEl = document.createElement('input-el')
+const inputEl = document.getElementById('input-el')
 const inputLestener = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+
 
 inputLestener.addEventListener("click",function(){
     myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderElemnts()
 })
 
+function renderElemnts(){
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems+= "<li><a href =" + myLeads[i] +"target='_blank'>" + myLeads[i] + " </a></li>" ;
+    }
+    ulEl.innerHTML = listItems
 
 
-
-for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
 }
