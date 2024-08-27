6d1a5d8e2d1e0f91cbad23c6e526fe7a848cd989
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-08-27 18:01:35 +0300

    Added Json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let myLeads = []
 
-const inputEl = document.getElementById('input-el')
+const inputEl = document.getElementById(`input-el`)
 const inputLestener = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
@@ -16,7 +16,7 @@ function renderElemnts(){
     let listItems = ""
 
     for (let i = 0; i < myLeads.length; i++) {
-        listItems+= "<li><a href =" + myLeads[i] +"target='_blank'>" + myLeads[i] + " </a></li>" ;
+        listItems+= `<li><a href = ${myLeads[i]} target="_blank">${myLeads[i]} </a></li>` 
     }
     ulEl.innerHTML = listItems
 
