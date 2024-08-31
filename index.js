cd541e29b42528cdc5cdf3ed0448d0743a9a6d4e
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-09-01 02:31:25 +0300

    Fixed Spelling Mistakes

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
 let myLeads = []
 
-const inputEl = document.getElementById(`input-el`)
-const inputLestener = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const inputListener = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const tabBtn = document.getElementById("tab-btn")
 
-if (leadsFromLocalStorage){
+if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderElemnts(myLeads)
+    renderElements(myLeads)
 }
 
 const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+    { url: "https://www.linkedin.com/in/per-harald-borgen/" }
 ]
 
-tabBtn.addEventListener("click",function(){
-    chrome.tabs.query({active:true, currentwindow:true}, function(tabs){
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads",JSON.stringify(myLeads))
-        renderElemnts(myLeads)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderElements(myLeads)
     })
 })
 
-deleteBtn.addEventListener("click",function(){
+deleteBtn.addEventListener("click", function() {
     localStorage.clear()
     myLeads = []
-    renderElemnts(myLeads)
+    renderElements(myLeads)
 })
 
-
-
-inputLestener.addEventListener("click",function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderElemnts(myLeads)
+inputListener.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderElements(myLeads)
 })
 
-function renderElemnts(links){
+function renderElements(links) {
     let listItems = ""
 
     for (let i = 0; i < links.length; i++) {
-        listItems+= `<li><a href = '${links[i]}' target="_blank">${links[i]} </a></li>` 
+        listItems += `<li><a href='${links[i]}' target="_blank">${links[i]}</a></li>`
     }
     ulEl.innerHTML = listItems
-
-
 }
