baad9f9da9262795f6269190a7d3818e95bd04f0
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-09-01 02:26:25 +0300

    Final Version

--- index.js
+++ index.js
@@ -3,20 +3,47 @@ let myLeads = []
 const inputEl = document.getElementById(`input-el`)
 const inputLestener = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderElemnts(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active:true, currentwindow:true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        renderElemnts(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("click",function(){
+    localStorage.clear()
+    myLeads = []
+    renderElemnts(myLeads)
+})
 
 
 
 inputLestener.addEventListener("click",function(){
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    renderElemnts()
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderElemnts(myLeads)
 })
 
-function renderElemnts(){
+function renderElemnts(links){
     let listItems = ""
 
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems+= `<li><a href = ${myLeads[i]} target="_blank">${myLeads[i]} </a></li>` 
+    for (let i = 0; i < links.length; i++) {
+        listItems+= `<li><a href = '${links[i]}' target="_blank">${links[i]} </a></li>` 
     }
     ulEl.innerHTML = listItems
 
